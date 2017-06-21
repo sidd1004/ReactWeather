54b4bc2676bd7ffe60ff330a06a242e7b2e77992
Siddharth <siddharth_1004@live.com>
2017-06-21 15:09:41 +0530

    Heroku  changes

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-porto'] === 'http') {
+        next();
+    }
+    else {
+        res.redirect('http://' + req.hostmame + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log("Server Started");
 });
