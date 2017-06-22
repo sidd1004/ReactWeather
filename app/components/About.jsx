853ae05b6ec443e93a8227560952031a5feccc78
Siddharth <siddharth_1004@live.com>
2017-06-22 15:01:30 +0530

    about page changes

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,25 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 var About = (props) => {
     return (
         <div>
-            <h3>About Component</h3>
+            <h1 className="text-center">About</h1>
+            <p>Tools Used:</p>
+            <ol>
+                <li>
+                    <a href="https://facebook.github.io/react/" target="_blank">React</a>
+                </li>
+                <li>
+                    <a href="https://foundation.zurb.com/" target="_blank">Foundation</a>
+                </li>
+                <li>
+                    <a href="https://webpack.js.org/" target="_blank">Webpack</a>
+                </li>
+                <li>
+                    <a href="https://openweathermap.org/" target="_blank">Open Weather Map</a>
+                </li>
+            </ol>
         </div>
     );
 };
